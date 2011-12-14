c2de626c4a3db3acacc72069500ce83648a90399
xumiao <xmcpp@139.com>
2011-12-14 22:19:11 +0800

    增加了对lua函数调用的单元测试

--- UnitTest/ScriptTestSuite.h
+++ UnitTest/ScriptTestSuite.h
@@ -11,12 +11,32 @@ public:
 	static void TearDownTestCase();
 };
 
-
+/**测试是否可以读取文件类型的脚本*/
 TEST_F( ScriptTestSuite , ScriptLoadFileTest )
 {
+	int oldTop = ScriptSystem::getSingleton().getLuaStateTop(); //获得调用之前栈顶序号
+
 	EXPECT_TRUE( ScriptSystem::getSingleton().executeFile( "test.lua" ) );
-	EXPECT_TRUE( ScriptSystem::getSingleton().executeGlobalFunction( "cppgurutest" ) );
 	
+	int newTop = ScriptSystem::getSingleton().getLuaStateTop(); //获得调用之后栈顶序号
+	EXPECT_TRUE( oldTop == newTop ); //如没有错误则应该相等，否则在某个分支上未恢复栈顶，出现泄露。
+}
+
+/**测试是否可以调用全局函数，调用返回1个bool类型的函数*/
+TEST_F( ScriptTestSuite , ScriptCallFunctionWithBoolRet )
+{
+	int oldTop = ScriptSystem::getSingleton().getLuaStateTop();
+
+	EXPECT_TRUE( ScriptSystem::getSingleton().executeFile( "test.lua" ) );
+	EXPECT_TRUE( ScriptSystem::getSingleton().executeGlobalFunction( "setBoolValue" ) );
+	bool ret = false;
+	EXPECT_TRUE( ScriptSystem::getSingleton().executeGlobalFunction( "getBoolValue" , ret ) );
+	EXPECT_TRUE( ret );
+
+	EXPECT_FALSE( ScriptSystem::getSingleton().executeGlobalFunction( "getIntValue" , ret ) );
+
+	int newTop = ScriptSystem::getSingleton().getLuaStateTop();
+	EXPECT_TRUE( oldTop == newTop );
 }
 
 #endif //__SCRIPTTESTSUITE_H__
