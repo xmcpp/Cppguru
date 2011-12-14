4abc3c5c643cb065a1a354c8a6972d746a4617b4
xumiao <xmcpp@139.com>
2011-12-14 14:00:56 +0800

    1.实现了不带参数的lua函数调用 2.实现了lua文件调用

--- ScriptSystem.cpp
+++ ScriptSystem.cpp
@@ -9,6 +9,8 @@ extern "C"
 
 };
 
+template<> ScriptSystem* Singleton<ScriptSystem>::ms_Singleton	= 0;
+
 ScriptSystem::ScriptSystem()
 :m_state(NULL)
 {
@@ -20,3 +22,39 @@ ScriptSystem::~ScriptSystem()
 	lua_close( m_state );
 }
 
+bool ScriptSystem::executeGlobalFunction( const std::string & name )
+{
+	int top = lua_gettop( m_state );
+
+	//获得指定名称的函数
+	lua_getglobal( m_state , name.c_str() );
+	
+	//检测是否是函数，-1表示栈顶
+	if( !lua_isfunction( m_state , -1 ) )
+	{
+		return false;
+	}
+
+	//调用,无入参，无出参
+	lua_call( m_state , 0 , 0 );
+	
+	lua_settop( m_state , top );
+	return true;
+}
+
+bool ScriptSystem::executeFile( const std::string & fileName )
+{
+	int top = lua_gettop( m_state );
+	
+	//加载脚本文件
+	if( luaL_loadfile( m_state , fileName.c_str() ) )
+	{
+		return false;
+	}
+	
+	//调用,无入参，无出参
+	lua_call( m_state , 0 , 0 );
+	
+	lua_settop( m_state , top );
+	return true;
+}
--- ScriptSystem.h
+++ ScriptSystem.h
@@ -1,15 +1,31 @@
+/**********************************************************************
+脚本系统
+
+Desc:
+	本对象包装lua库的使用方法,提供简单的启动和调用lua脚本中函数的功能.
+
+Author: 徐淼
+
+Date: 2011.12.14
+
+Update: 
+
+***********************************************************************/
 #ifndef __SCRIPTSYSTEM_H__
 #define __SCRIPTSYSTEM_H__
 
+#include "Singleton.h"
 struct lua_State;
 
-class ScriptSystem
+class ScriptSystem : public Singleton<ScriptSystem>
 {
 public:
 	ScriptSystem();
 	~ScriptSystem();
 public:
+	bool executeGlobalFunction( const std::string & name );
 
+	bool executeFile( const std::string & fileName );
 private:
 	lua_State * m_state;
 };
