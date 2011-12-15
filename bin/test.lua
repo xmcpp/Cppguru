5c0b846dc98249b066a15bdf75c6ea853805e68a
xumiao <xmcpp@139.com>
2011-12-15 17:18:10 +0800

    添加了表的代码.

--- bin/test.lua
+++ bin/test.lua
@@ -1,20 +1,22 @@
 local value = false;
 
+--测试是否可以调用无返回值,无残书的函数
 function setBoolValue()
 	value = true;
 end;
 
+--测试局部变量value是否被正确设置
 function getBoolValue()
     return value;
 end;
 
-
+--测试是否可以调用返回值的函数
 function getIntValue()
     return 1;
 end;
 
-t = {};
-
-function t.a()
-
+--测试是否可以调用表中的函数
+a = {};
+function a.b()
+    value = true;
 end;
