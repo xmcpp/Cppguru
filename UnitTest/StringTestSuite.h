42c947de10b37b685643e964948609e753c138ed
xumiao <xmcpp@139.com>
2012-01-05 17:50:03 +0800

    添加去空格函数测试用例

--- UnitTest/StringTestSuite.h
+++ UnitTest/StringTestSuite.h
@@ -43,4 +43,52 @@ TEST(StringToolTest , SplitTest4)
 	ASSERT_EQ( 1 , stringVec.size() );
 	EXPECT_EQ( "a" , stringVec[0] );
 }
+
+TEST(StringToolTest , TrimTest)
+{
+	std::string test = "  abc  ";
+	StringTools::trimBoth( test );
+	EXPECT_EQ( "abc" , test );
+
+	test = "";
+	StringTools::trimBoth( test );
+	EXPECT_EQ( "" , test );
+
+	test = "  a  ";
+	StringTools::trimBoth( test );
+	EXPECT_EQ( "a" , test );
+
+	test = "  a b c  ";
+	StringTools::trimBoth( test );
+	EXPECT_EQ( "a b c" , test );
+
+	test = "  abc";
+	StringTools::trimBoth( test );
+	EXPECT_EQ( "abc" , test );
+
+	test = "abc ";
+	StringTools::trimBoth( test );
+	EXPECT_EQ( "abc" , test );
+
+	test = "abc";
+	StringTools::trimBoth( test );
+	EXPECT_EQ( "abc" , test );
+
+	test = "a";
+	StringTools::trimBoth( test );
+	EXPECT_EQ( "a" , test );
+
+	test = " ";
+	StringTools::trimBoth( test );
+	EXPECT_EQ( "" , test );
+
+	test = "  ";
+	StringTools::trimBoth( test );
+	EXPECT_EQ( "" , test );
+
+	test = "   ";
+	StringTools::trimBoth( test );
+	EXPECT_EQ( "" , test );
+}
+
 #endif //__STRINGTESTSUITE_H__
