ca153d75c0ff9a19af14475f865659cb54fd2b57
xumiao <xmcpp@139.com>
2012-01-12 21:54:59 +0800

    完善基于boost的线程对象

--- BoostThread.cpp
+++ BoostThread.cpp
@@ -3,7 +3,7 @@
 
 
 BoostThread::BoostThread()
-:m_thread(NULL),m_isRunning(false)
+:m_thread(NULL),m_bStop(false)
 {
 	
 }
@@ -19,6 +19,7 @@ bool BoostThread::beginThread()
 	if( m_thread ) return false;
 
 	m_thread = new boost::thread( boost::bind(&BoostThread::threadProc,this) );
+	m_bStop = false;
 	return true;
 }
 
@@ -26,12 +27,23 @@ bool BoostThread::endThread()
 {
 	if( !m_thread) return true;
 	
-	
+	m_bStop = true;
+	m_thread->join();
+
+	delete m_thread;
+	m_thread = NULL;
 
 	return true;
 }
 
 void BoostThread::threadProc()
 {
+	onBegin();
+	
+	while(!m_bStop)
+	{
+		m_bStop = !onProcess();
+	}
 
+	onEnd();
 }
--- BoostThread.h
+++ BoostThread.h
@@ -1,3 +1,15 @@
+/**********************************************************************
+线程基类
+
+基于boost的线程库实现的跨平台线程对象。通过继承该类使对象拥有多线程
+的能力，并在onProcess函数中实现功能。
+
+Author: 徐淼
+
+Date: 2012.1.12
+
+Update: 
+***********************************************************************/
 #ifndef __BOOSTTHREAD_H__
 #define __BOOSTTHREAD_H__
 
@@ -7,16 +19,30 @@ class BoostThread
 {
 public:
 	BoostThread();
-	~BoostThread();
+	virtual ~BoostThread();
 public:
+	/**启动线程 */
 	bool beginThread();
+
+	/**停止线程 */
 	bool endThread();
-	
+public:
+	/**用于线程初始化 */
+	virtual void onBegin(){}
+
+	/**用于线程清理 */
+	virtual void onEnd(){}
+
+	/**用于线程执行
+	 *该函数会被无限次调用，除非调用endTread方法或该函数返回false
+	 *@return 如果该函数返回false则线程执行结束
+	 */
+	virtual bool onProcess(){ return true; }
 protected:
 	void threadProc();
 protected:
 	boost::thread * m_thread;
-	bool m_isRunning;
+	bool m_bStop;
 };
 
 #endif //__BOOSTTHREAD_H__
